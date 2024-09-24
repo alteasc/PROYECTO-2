5551ea6358a89f302cf7a18aa25dbb8ba2d64896
alteaSC <seguracaliz@gmail.com>
2024-09-25 01:56:38 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,481 @@
+const ROPA = [
+  {
+    nombre: 'Abrigo Cozy',
+    prenda: 'Abrigo',
+    color: ['aquamarine'],
+    precio: 92.5,
+    imagen: './assets/abrigo-bebe-nina.webp',
+    talla: ['0 a 24 meses'],
+    sexo: 'niña'
+  },
+  {
+    nombre: 'Abrigo Puntos',
+    prenda: 'Abrigo',
+    color: ['white', 'red', 'blue'],
+    precio: 87.5,
+    imagen: './assets/abrigo-bebe-nina2.webp',
+    talla: ['0 a 24 meses'],
+    sexo: 'niña'
+  },
+  {
+    nombre: 'Abrigo Parka',
+    prenda: 'Abrigo',
+    color: ['grey'],
+    precio: 99.95,
+    imagen: './assets/abrigo-bebe-nino.webp',
+    talla: ['0 a 24 meses'],
+    sexo: 'niño'
+  },
+  {
+    nombre: 'Camiseta Buzo',
+    prenda: 'Camiseta',
+    color: ['white', 'green'],
+    precio: 34.95,
+    imagen: './assets/camiseta-nina.webp',
+    talla: ['24 meses a 5 años'],
+    sexo: 'niña'
+  },
+  {
+    nombre: 'Camiseta Patinetes',
+    prenda: 'Camiseta',
+    color: ['yellow'],
+    precio: 32.5,
+    imagen: './assets/camiseta-nino.webp',
+    talla: ['24 meses a 5 años'],
+    sexo: 'nino'
+  },
+  {
+    nombre: 'Pantalón Corto Lazo',
+    prenda: 'Pantalon',
+    color: ['yellow', 'green'],
+    precio: 28.95,
+    imagen: './assets/pantalon-corto-nina.webp',
+    talla: ['24 meses a 5 años'],
+    sexo: 'niña'
+  },
+  {
+    nombre: 'Pantalón Corto Nudo',
+    prenda: 'Pantalon',
+    color: ['blue'],
+    precio: 35.95,
+    imagen: './assets/pantalon-corto-nino.webp',
+    talla: ['24 meses a 5 años'],
+    sexo: 'niño'
+  },
+  {
+    nombre: 'Pantalón Largo Acampanado',
+    prenda: 'Pantalon',
+    color: ['blue'],
+    precio: 49.95,
+    imagen: './assets/pantalon-largo-nina.webp',
+    talla: ['24 meses a 5 años'],
+    sexo: 'niña'
+  },
+  {
+    nombre: 'Pantalón Largo Marine',
+    prenda: 'Pantalon',
+    color: ['blue'],
+    precio: 34.95,
+    imagen: './assets/pantalon-largo-nino.webp',
+    talla: ['24 meses a 5 años'],
+    sexo: 'niño'
+  },
+  {
+    nombre: 'Pelele Arcoiris',
+    prenda: 'Pelele',
+    color: ['pink', 'yellow', 'green', 'grey'],
+    precio: 54.95,
+    imagen: './assets/pelele-arcoiris-bebe-nina.webp',
+    talla: ['0 a 24 meses'],
+    sexo: 'niña'
+  },
+  {
+    nombre: 'Peto Fresas',
+    prenda: 'Peto',
+    color: ['pink', 'white'],
+    precio: 43.5,
+    imagen: './assets/peto-fresas-bebe-nina.webp',
+    talla: ['0 a 24 meses'],
+    sexo: 'niña'
+  },
+  {
+    nombre: 'Pelele Combinado',
+    prenda: 'Pelele',
+    color: ['white', 'aquamarine'],
+    precio: 54.95,
+    imagen: './assets/pelele-combinado-bebe-nino.webp',
+    talla: ['0 a 24 meses'],
+    sexo: 'niño'
+  },
+  {
+    nombre: 'Pelele Largo Marrón',
+    prenda: 'Pelele',
+    color: ['brown'],
+    precio: 64.95,
+    imagen: './assets/pelele-marron-bebe-nino.webp',
+    talla: ['0 a 24 meses'],
+    sexo: 'niño'
+  },
+  {
+    nombre: 'Peto Azul',
+    prenda: 'Peto',
+    color: ['blue'],
+    precio: 48.95,
+    imagen: './assets/peto-azul-bebe-nino.webp',
+    talla: ['0 a 24 meses'],
+    sexo: 'niño'
+  },
+  {
+    nombre: 'Sudadera Conejitos',
+    prenda: 'Sudadera',
+    color: ['pink', 'blue'],
+    precio: 37.95,
+    imagen: './assets/sudadera-nina.webp',
+    talla: ['24 meses a 5 años'],
+    sexo: 'niña'
+  },
+  {
+    nombre: 'Sudadera Conejitos',
+    prenda: 'Sudadera',
+    color: ['grey', 'blue'],
+    precio: 34.95,
+    imagen: './assets/sudadera-nino.webp',
+    talla: ['24 meses a 5 años'],
+    sexo: 'niño'
+  },
+  {
+    nombre: 'Sudadera Capitán',
+    prenda: 'Sudadera',
+    color: ['yellow'],
+    precio: 39.95,
+    imagen: './assets/sudadera-nino2.webp',
+    talla: ['24 meses a 5 años'],
+    sexo: 'niño'
+  },
+  {
+    nombre: 'Vestido Flores',
+    prenda: 'Vestido',
+    color: ['yellow', 'pink', 'green'],
+    precio: 64.95,
+    imagen: './assets/vestido-flores-nina.webp',
+    talla: ['24 meses a 5 años'],
+    sexo: 'niña'
+  },
+  {
+    nombre: 'Vestido Rayas Rojas',
+    prenda: 'Vestido',
+    color: ['red', 'white'],
+    precio: 67.95,
+    imagen: './assets/vestido-rayas-nina.webp',
+    talla: ['24 meses a 5 años'],
+    sexo: 'niña'
+  },
+  {
+    nombre: 'Vestido Volantes',
+    prenda: 'Vestido',
+    color: ['blue', 'pink', 'white', 'green'],
+    precio: 64.95,
+    imagen: './assets/vestido-volantes-nina.webp',
+    talla: ['24 meses a 5 años'],
+    sexo: 'niña'
+  },
+  {
+    nombre: 'Vestido Combinado',
+    prenda: 'Vestido',
+    color: ['pink', 'white'],
+    precio: 59.95,
+    imagen: './assets/vestido-rosa-bebe-nina.webp',
+    talla: ['0 a 24 meses'],
+    sexo: 'niña'
+  },
+  {
+    nombre: 'Vestido Blanco Dibujos',
+    prenda: 'Vestido',
+    color: ['white', 'orange', 'green'],
+    precio: 59.95,
+    imagen: './assets/vestido-blanco-bebe-nina.webp',
+    talla: ['0 a 24 meses'],
+    sexo: 'niña'
+  },
+  {
+    nombre: 'Vestido Rayas Negras',
+    prenda: 'Vestido',
+    color: ['white', 'black'],
+    precio: 62.95,
+    imagen: './assets/vestido-rayas-bebe-nina.webp',
+    talla: ['0 a 24 meses'],
+    sexo: 'niña'
+  },
+  {
+    nombre: 'Camisa Avionetas',
+    prenda: 'Camisa',
+    color: ['yellow', 'blue'],
+    precio: 46.5,
+    imagen: './assets/camisa-avionetas-bebe-nino.webp',
+    talla: ['0 a 24 meses'],
+    sexo: 'niño'
+  },
+  {
+    nombre: 'Camisa Pajarita',
+    prenda: 'Camisa',
+    color: ['white'],
+    precio: 43.5,
+    imagen: './assets/camisa-pajarita-bebe-nino.webp',
+    talla: ['0 a 24 meses'],
+    sexo: 'niño'
+  },
+  {
+    nombre: 'Braga Flores',
+    prenda: 'Braga',
+    color: ['white', 'pink'],
+    precio: 19.95,
+    imagen: './assets/braga-flores-bebe-nina.webp',
+    talla: ['0 a 24 meses'],
+    sexo: 'niña'
+  },
+  {
+    nombre: 'Braga Cuadros',
+    prenda: 'Braga',
+    color: ['red', 'white'],
+    precio: 19.95,
+    imagen: './assets/braga-cuadros-bebe-nina.webp',
+    talla: ['0 a 24 meses'],
+    sexo: 'niña'
+  },
+  {
+    nombre: 'Pantalón Corto Avionetas',
+    prenda: 'Pantalon',
+    color: ['yellow', 'blue'],
+    precio: 29.95,
+    imagen: './assets/pantalon-corto-bebe-nino.webp',
+    talla: ['0 a 24 meses'],
+    sexo: 'niño'
+  }
+]
+
+const PRENDAS = []
+const arrayColores = []
+let PRENDA = ''
+let COLOR = ''
+
+const main = document.querySelector('main')
+const divFiltros = document.createElement('div')
+divFiltros.classList.add('filtros')
+divFiltros.setAttribute('id', 'no-visible')
+const seccionProductos = document.querySelector('#productos')
+seccionProductos.insertAdjacentElement('beforebegin', divFiltros)
+
+// PINTAR BLOQUE SUGERENCIAS Y 3 PRODUCTOS ALEATORIOS
+const productosRandom = () => {
+  const divSuggest = document.createElement('div')
+  const pSugerencias = document.createElement('p')
+  const p = document.createElement('p')
+  divSuggest.id = 'sugerencias'
+  pSugerencias.textContent = 'No hay productos disponibles para tu búsqueda'
+  p.className = 'our-suggest'
+  p.textContent = 'Aquí te dejamos nuestras sugerencias...'
+
+  const productosSugeridos = []
+
+  for (let i = 0; i < 3; i++) {
+    const element = ROPA[Math.floor(Math.random() * ROPA.length)]
+    productosSugeridos.push(element)
+    console.log(productosSugeridos)
+  }
+  printRopa(productosSugeridos)
+
+  divSuggest.appendChild(pSugerencias)
+  divSuggest.appendChild(p)
+  seccionProductos.insertAdjacentElement('beforebegin', divSuggest)
+}
+
+// RESETEAR FILTROS
+const resetFiltros = () => {
+  printRopa(ROPA)
+  const selectPrendas = document.querySelector('.menu-prendas')
+  selectPrendas.value = 'selecciona'
+  location.reload()
+}
+
+// FILTRAR ROPA POR COLORES
+const filtrarColores = () => {
+  const filtered = []
+
+  for (const ropa of ROPA) {
+    if (ropa.color.includes(COLOR)) {
+      filtered.push(ropa)
+    }
+  }
+  printRopa(filtered)
+  console.log(filtered)
+}
+
+// FILTRAR ROPA POR COLORES Y PRENDAS
+const allFilter = () => {
+  const filtrados = []
+
+  for (const ropa of ROPA) {
+    if (PRENDA.includes(ropa.prenda) && ropa.color.includes(COLOR)) {
+      filtrados.push(ropa)
+    }
+  }
+  seccionProductos.innerHTML = ''
+  printRopa(filtrados)
+  console.log(filtrados)
+  if (filtrados.length < 1) {
+    seccionProductos.innerHTML = ''
+    productosRandom()
+  }
+}
+
+// FUNCIONES PARA RECOGER LOS DATOS DE CADA FILTRO
+const fillPrendas = (prendas) => {
+  PRENDAS.splice(0)
+  for (const prenda of prendas) {
+    if (!PRENDAS.includes(prenda.prenda)) {
+      PRENDAS.push(prenda.prenda)
+    }
+  }
+}
+fillPrendas(ROPA)
+
+const fillColores = () => {
+  for (const ropa of ROPA) {
+    const coloresPrendas = ropa.color
+
+    for (const colorPrenda of coloresPrendas) {
+      const COLORES = colorPrenda
+      if (!arrayColores.includes(COLORES)) {
+        arrayColores.push(COLORES)
+      }
+    }
+  }
+}
+fillColores()
+
+// PINTAR BLOQUE FILTROS Y FUNCIONALIDAD DE CADA ELEMENTO
+const filtrosTienda = () => {
+  const selectPrenda = document.createElement('select')
+  selectPrenda.classList.add('menu-prendas')
+  const title = document.createElement('option')
+  title.textContent = 'Selecciona'
+  title.disabled = false
+  title.select = true
+  selectPrenda.appendChild(title)
+  const btnFiltrar = document.createElement('button')
+  btnFiltrar.textContent = 'Filtrar'
+  btnFiltrar.id = 'boton-filtrar'
+  const buttonReset = document.createElement('button')
+  buttonReset.textContent = 'Limpiar filtros'
+  buttonReset.id = 'boton-reset'
+  buttonReset.type = 'reset'
+
+  for (const prenda of PRENDAS) {
+    const option = document.createElement('option')
+
+    option.value = prenda
+    option.textContent = prenda
+
+    selectPrenda.appendChild(option)
+  }
+  divFiltros.appendChild(selectPrenda)
+
+  const ulColores = document.createElement('ul')
+  ulColores.classList.add('menu-colores')
+
+  for (const color of arrayColores) {
+    const liColor = document.createElement('li')
+    const divColor = document.createElement('div')
+    const inputColor = document.createElement('input')
+    const pColor = document.createElement('p')
+
+    divColor.style.backgroundColor = color
+    inputColor.type = 'radio'
+    inputColor.name = 'color'
+    inputColor.id = 'colores'
+    inputColor.value = color
+    pColor.textContent = color
+
+    ulColores.appendChild(liColor)
+    liColor.appendChild(divColor)
+    liColor.appendChild(inputColor)
+    liColor.appendChild(pColor)
+  }
+
+  divFiltros.appendChild(ulColores)
+  divFiltros.appendChild(btnFiltrar)
+  divFiltros.appendChild(buttonReset)
+
+  selectPrenda.addEventListener('change', (e) => {
+    const copiaROPA = [...ROPA]
+    PRENDA = e.target.value
+    const filtradosPrendas = copiaROPA.filter((copiaropa) =>
+      PRENDA.includes(copiaropa.prenda)
+    )
+    printRopa(filtradosPrendas)
+  })
+
+  ulColores.addEventListener('click', (e) => {
+    COLOR = e.target.value
+    filtrarColores()
+  })
+
+  btnFiltrar.addEventListener('click', allFilter)
+
+  buttonReset.addEventListener('click', resetFiltros)
+}
+
+filtrosTienda()
+
+// PINTAR BLOQUE PRODUCTOS
+const printRopa = (ropa) => {
+  const seccionProductos = document.querySelector('#productos')
+  seccionProductos.innerHTML = ''
+
+  for (const producto of ropa) {
+    const divProducto = document.createElement('div')
+    const img = document.createElement('img')
+    const nombre = document.createElement('h3')
+    const precio = document.createElement('p')
+
+    img.src = producto.imagen
+    nombre.textContent = producto.nombre
+    precio.textContent = `${producto.precio}€`
+    divProducto.classList.add(producto.prenda)
+
+    seccionProductos.appendChild(divProducto)
+    divProducto.appendChild(img)
+    divProducto.appendChild(nombre)
+    divProducto.appendChild(precio)
+  }
+}
+printRopa(ROPA)
+
+// MENÚ FILTROS
+const btnFilterMenu = document.querySelector('#btn-filter')
+btnFilterMenu.textContent = 'FILTROS'
+let FilterMenuVisible = false
+
+const toggleMenuFiltros = () => {
+  if (FilterMenuVisible) {
+    divFiltros.setAttribute('id', 'no-visible')
+    FilterMenuVisible = false
+  } else {
+    divFiltros.removeAttribute('id', 'no-visible')
+    FilterMenuVisible = true
+  }
+}
+btnFilterMenu.addEventListener('click', toggleMenuFiltros)
+
+// MENÚ HAMBURGUESA
+const nav = document.querySelector('.navbar')
+const btnAbrir = document.querySelector('#abrir-menu')
+const btnCerrar = document.querySelector('#cerrar-menu')
+
+btnAbrir.addEventListener('click', () => {
+  nav.classList.add('visible')
+})
+
+btnCerrar.addEventListener('click', () => {
+  nav.classList.remove('visible')
+})
